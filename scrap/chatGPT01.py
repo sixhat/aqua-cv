277af56c8179baf8626436c35637f0ad855b0b23
Dave <sixhat@users.noreply.github.com>
2023-09-29 23:20:51 +0100

    Update chatGPT01.py

--- scrap/chatGPT01.py
+++ scrap/chatGPT01.py
@@ -1,10 +1,10 @@
 # Decided to ask chatGPT how to do it...
 # This is a simple color-based segmentation to identify possible obstacles. 
 
-import cv2
+import cv2 as cv
 import numpy as np
 
-cap = cv2.VideoCapture(0)  # Use 0 for the default camera (usually the webcam)
+cap = cv.VideoCapture(0)  # Use 0 for the default camera (usually the webcam)
 
 while True:
     ret, frame = cap.read()
@@ -16,24 +16,24 @@ while True:
     lower_orange = np.array([0, 100, 100])
     upper_orange = np.array([20, 255, 255])
     # Convert the frame to the HSV color space
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # Create a mask for the orange color
-    mask = cv2.inRange(hsv_frame, lower_orange, upper_orange)
+    mask = cv.inRange(hsv_frame, lower_orange, upper_orange)
     # Find contours in the mask
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     # Draw bounding boxes around detected obstacles
     for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
+        x, y, w, h = cv.boundingRect(contour)
         if (w * h > 1000):
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.circle(frame,  (x + w//2, y + h//2), 10, (255, 255, 0), 2)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv.circle(frame,  (x + w//2, y + h//2), 10, (255, 255, 0), 2)
     # End of obstacle detection
     
     
-    cv2.imshow('Obstacle Avoidance', frame)
+    cv.imshow('Obstacle Avoidance', frame)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
